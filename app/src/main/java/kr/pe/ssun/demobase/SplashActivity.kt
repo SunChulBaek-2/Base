6eb0bf06a708109a3cec2e4094de10e6c985edf0
x1210x <x1210x@gmail.com>
2017-11-27 17:16:09 +0900

    initial commit

---
+++ app/src/main/java/kr/pe/ssun/demobase/SplashActivity.kt
@@ -0,0 +1,14 @@
+package kr.pe.ssun.demobase
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by sunchul.baek on 11월 27일.
+ */
+class SplashActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
